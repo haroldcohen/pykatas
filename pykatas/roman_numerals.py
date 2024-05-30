60c08d190e68fe980f67182303b22d8586005dd3
Harold Cohen <h.cohen.apple@me.com>
2024-05-30 12:48:47 +0200

    refactor(roman): remove number param from conversion methods

--- pykatas/roman_numerals.py
+++ pykatas/roman_numerals.py
@@ -16,18 +16,18 @@ class RomanNumeralsConverter:
     def convert(self) -> str:
         return (
             "M" * int(self._number / 1000)
-            + self._convert(numeration=100, number=self._number % 1000)
-            + self._convert(numeration=10, number=self._number % 100)
-            + self._convert(numeration=1, number=self._number % 10)
+            + self._convert(numeration=100)
+            + self._convert(numeration=10)
+            + self._convert(numeration=1)
         )
 
-    def _convert(self, numeration: int, number: int):
+    def _convert(self, numeration: int):
+        self._number %= numeration * 10
         numeration_symbol = RomanNumeralsConverter._LOOKUP[numeration]
         half_symbol = RomanNumeralsConverter._LOOKUP[numeration * 5]
-        if number >= numeration * 5:
+        if self._number >= numeration * 5:
             next_numeration = numeration * 10
             return self._to_upper_half(
-                number=number,
                 numeration=numeration,
                 next_numeration=next_numeration,
                 numeration_symbol=numeration_symbol,
@@ -35,33 +35,30 @@ class RomanNumeralsConverter:
                 half_symbol=half_symbol,
             )
         return self._to_lower_half(
-            number=number,
             numeration=numeration,
             numeration_symbol=numeration_symbol,
             half_symbol=half_symbol,
         )
 
-    def _to_lower_half(
-        self,
-        number: int,
-        numeration: int,
-        numeration_symbol: str,
-        half_symbol: str,
-    ):
-        if number % (5 * numeration) >= (4 * numeration):
-            return numeration_symbol + half_symbol
-        return numeration_symbol * int(number / numeration)
-
     def _to_upper_half(
         self,
-        number: int,
         numeration: int,
         next_numeration: int,
         numeration_symbol: str,
         next_numeration_symbol: str,
         half_symbol: str,
     ) -> str:
-        if number >= next_numeration - numeration:
+        if self._number >= next_numeration - numeration:
             return numeration_symbol + next_numeration_symbol
-        remainder = number - next_numeration / 2
+        remainder = self._number - next_numeration / 2
         return half_symbol + numeration_symbol * int(remainder / numeration)
+
+    def _to_lower_half(
+        self,
+        numeration: int,
+        numeration_symbol: str,
+        half_symbol: str,
+    ):
+        if self._number % (5 * numeration) >= (4 * numeration):
+            return numeration_symbol + half_symbol
+        return numeration_symbol * int(self._number / numeration)
